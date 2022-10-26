2265566a337b7a823656b75f2b83821b010f3606
yingzh <Ying.Zhang@metropolia.fi>
2022-10-26 15:14:11 +0300

    adding pug page /test

--- server-one/index.js
+++ server-one/index.js
@@ -8,6 +8,7 @@ const port = 3000;
 let requestCounter = 0;
 
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
@@ -25,7 +26,15 @@ app.get('/catinfo', (req, res) => {
 app.get('/test', (request, response) => {
     console.log('Someone is trying to test me.');
     requestCounter++;
-    response.send('<h1>TEXT page</h1><p>' + requestCounter + '<p>');
+    // Example of using pug
+    response.render('test', {
+        title:"Pug test page",
+        header1:"Pug test page",
+        header2:"Counter",
+        exampleText:"Page requested" + requestCounter + " times.",
+    });
+    // basic html as string
+    //response.send('<h1>TEXT page</h1><p>' + requestCounter + '<p>');
 });
 
 app.listen(port, () => {
