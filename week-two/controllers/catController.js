7a70589fb24c65d0f32e32d4845fea8d6353735b
yingzh <Ying.Zhang@metropolia.fi>
2022-11-02 09:28:59 +0200

    add the catController

--- week-two/controllers/catController.js
+++ week-two/controllers/catController.js
@@ -1,2 +1,33 @@
 'use strict';
 // catController
+const catModel = require('../models/catModel');
+
+const cats = catModel.cats;
+
+const getCats = (req, res) => {
+    res.json(cats);
+};
+
+const getCat = (req, res) => {
+    
+};
+
+const modifyCat = (req, res) => {
+    
+};
+
+const createCat = (req, res) => {
+    
+};
+
+const deleteCat = (req, res) => {
+    
+};
+
+module.exports = {
+    getCat,
+    getCats,
+    modifyCat,
+    createCat,
+    deleteCat
+};
--- week-two/routes/catRoute.js
+++ week-two/routes/catRoute.js
@@ -2,26 +2,28 @@
 // catRoute
 const express = require('express');
 const router = express.Router();
+const catController = require('../controllers/catController');
 
-router.get('/cat', (req, res) => {
-    res.send('From this endpoint you can get cats.')
-  });
+// router.get('/', (req, res) => {
+//     res.send('From this endpoint you can get cats.')
+//   });
+router.get('/', catController.getCats);
   
-router.get('/cat/:catId', (req, res) => {
+router.get('/:catId', (req, res) => {
     // console.log(req.params);
     res.send('From this endpoint you can get cat with id ' + req .params.catId)
   });
   
-router.post('/cat', (req, res) => {
+router.post('/', (req, res) => {
     // console.log(req);
     res.send('From this endpoint you can add more cats.')
   });
   
-router.put('/cat', (req, res) => {
+router.put('/', (req, res) => {
     res.send('From this endpoint you can edit cats.')
   });
   
-router.delete('/cat', (req, res) => {
+router.delete('/', (req, res) => {
     res.send('From this endpoint you can delete cats.')
 });
   
