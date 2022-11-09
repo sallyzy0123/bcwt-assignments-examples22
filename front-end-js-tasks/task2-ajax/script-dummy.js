1907aef30bacaf50e9f07eb11d9705bfce24b2a5
yingzh <Ying.Zhang@metropolia.fi>
2022-11-09 14:41:15 +0200

    front-end task 2 basic version done

---
+++ front-end-js-tasks/task2-ajax/script-dummy.js
@@ -0,0 +1,20 @@
+'use strict';
+console.log('hello you');
+
+const figCaptionElem = document.querySelector('figcaption');
+const imgElem = document.querySelector('img');
+
+const getData = async () => {
+    const response = await fetch('pics.json');
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    figCaptionElem.innerText = data[0].name;
+    imgElem.src = data[0].url;
+    imgElem.alt = data[0].description;
+};
+
+getData();
+
+
+console.log("last line of code");
---
+++ front-end-js-tasks/task2-ajax/script.js
@@ -0,0 +1,73 @@
+'use strict';
+const apiUrl = 'https://api.tvmaze.com/search/shows?q=';
+// get reference to DOM elements
+const form = document.querySelector('#search-form');
+const button = form.querySelector('button');
+const input = form.querySelector('input');
+const results = document.querySelector('#results');
+
+button.addEventListener('click', (event) => {
+    // do not submit the form to anywahere (no page refresh)
+    event.preventDefault();
+    // prevent the generic event listener at the bottom
+    event.stopPropagation();
+    const queryParam = input.value;
+    getTVSeriesData(queryParam);
+    if (input.value.length > 1) {
+        getTVSeriesData(input.value);
+    }
+});
+
+const renderResults = (data) => {
+    // clear existing results before appending new ones
+    results.innerHTML = '';
+    // loop through all search results
+    for (let i=0; i<data.length; i++) {
+        const h3 = document.createElement('h3');
+        h3.textContent = data[i].show.name;
+        const img = document.createElement('img');
+        img.src = data[i].show.image.medium;
+        results.append(h3);
+        results.append(img);
+        // TODO: render more data from the results
+    }
+};
+
+const getTVSeriesData = async (name) => {
+    try {
+        const response = await fetch(apiUrl + name);
+        const data = await response.json();
+        console.log('results: ', data);
+        renderResults(data);
+    } catch (error) {
+        console.log('network failture: ', error);
+    }
+
+}
+
+
+
+// generic event handling example
+document.addEventListener('click', (event) => {
+    console.log('mouth clicked somewhere on the page.', event)
+})
+
+// console.log('hello you');
+
+// const figCaptionElem = document.querySelector('figcaption');
+// const imgElem = document.querySelector('img');
+
+// const getData = async () => {
+//     const response = await fetch('pics.json');
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
+//     figCaptionElem.innerText = data[0].name;
+//     imgElem.src = data[0].url;
+//     imgElem.alt = data[0].description;
+// };
+
+// getData();
+
+
+// console.log("last line of code");
