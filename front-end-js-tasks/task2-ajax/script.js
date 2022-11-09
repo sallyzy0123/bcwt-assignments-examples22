7a859f87f6de89f075c1a4a62cd64233cbfdd386
yingzh <Ying.Zhang@metropolia.fi>
2022-11-09 15:26:26 +0200

    remove the nonsence code

--- front-end-js-tasks/task2-ajax/script.js
+++ front-end-js-tasks/task2-ajax/script.js
@@ -45,29 +45,7 @@ const getTVSeriesData = async (name) => {
 
 }
 
-
-
 // generic event handling example
 document.addEventListener('click', (event) => {
     console.log('mouth clicked somewhere on the page.', event)
 })
-
-// console.log('hello you');
-
-// const figCaptionElem = document.querySelector('figcaption');
-// const imgElem = document.querySelector('img');
-
-// const getData = async () => {
-//     const response = await fetch('pics.json');
-//     console.log(response);
-//     const data = await response.json();
-//     console.log(data);
-//     figCaptionElem.innerText = data[0].name;
-//     imgElem.src = data[0].url;
-//     imgElem.alt = data[0].description;
-// };
-
-// getData();
-
-
-// console.log("last line of code");
