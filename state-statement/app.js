f6d8508417d1a6c16b36ff414afa4a44c2f935e3
yingzh <Ying.Zhang@metropolia.fi>
2022-11-22 13:53:39 +0200

    cookie task done

--- state-statement/app.js
+++ state-statement/app.js
@@ -1,13 +1,32 @@
 'use strict';
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/getCookie', (req, res) => {
+  console.log(req.cookies);
+  res.send('your color choice was:' + req.cookies.color);
+});
+
+app.get('/deleteCookie', (req, res) => {
+  res.clearCookie('color');
+  res.send('color cookie deleted.');
+});
+
+
+app.get('/setCookie/:color', (req, res) => {
+  console.log(req.params.color);
+  res.cookie('color', req.params.color);
+  res.send('cookie set');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
